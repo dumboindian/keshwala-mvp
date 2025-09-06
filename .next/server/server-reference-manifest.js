52fa3e4faddc7b8813bf6f334155ec5b31d6d11b
dumboindian <5023156@it.fcrit.ac.in>
2025-09-06 21:06:27 +0530

    cleanup build artifacts

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"1BtwF0WhFrzyBzzVaPFhh/quCOqVQ644c5yAmgESrDY=\"}"
--- out/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1qns1yd/mNX4QDQe/FhOUU41sITj9k4/FVT5zJOTdPQ=\"\n}"
